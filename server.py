33cdb72e3f5a88a0983557d43171f706c680922e
KluevJakov <kluevja@gmail.com>
2022-06-05 04:58:28 +0200

    add responses

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-import time
+import re
 
 hostName = "localhost"
 serverPort = 8080
+regions = ['Автозаводский р-н',
+           'Приокский р-н',
+           'Ленинский р-н']
+
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -11,7 +15,11 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
-        self.wfile.write(bytes('{"message": "Hello world"}', "utf-8"))
+        if 'test' in self.path:
+            id = int(re.findall('[0-9]+', self.path)[0])
+            self.wfile.write(bytes('{"message": "Response from id=' + str(id) + '", '
+                                   '"region": "' + regions[id] +'"}', "utf-8"))
+
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
